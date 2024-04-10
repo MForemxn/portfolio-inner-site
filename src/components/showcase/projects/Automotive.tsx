f0c1d1d6c95dce6bc8422a054ff0bb77624c82f8
Mason Foreman <104833724+MForemxn@users.noreply.github.com>
2024-04-10 16:16:15 +1000

    Update Automotive.tsx

--- src/components/showcase/projects/Automotive.tsx
+++ src/components/showcase/projects/Automotive.tsx
@@ -43,90 +43,94 @@ const ArtProjects: React.FC<ArtProjectsProps> = (props) => {
                             myself, original sprite by kevink
                         </sub>
                     </p>
-                    <h2>@Bluey on Tiktok</h2>
-                    <br />
-                    <p>
-                        Once I got my car, I fell in love and started making short TikTok videos as an excuse...
-                    </p>
-                    <br />
-                    <div className="captioned-image">
-                        <img src={girlRun} alt="" />
-                        <p>
-                            <sub>
-                                <b>Figure 1:</b> Eight Frame Run cycle animated by
-                                myself, original sprite by kevink
-                            </sub>
-                        </p>
-                    </div>
-                    <p>
-                        I really started enjoying the work I was doing, and felt I
-                        was improving a lot. Around 2024, I started to even work on
-                        making a realistic racing simulator for driver training.
-                        This was just running Asseto Corsa, however was able to be
-                        customised into realistic environments and charecteristics.
-                    </p>
-                    <br />
-                    <h3>Video Links:</h3>
-                    <br />
-                    <ul>
-                        <li>
-                            <a
-                                rel="noreferrer"
-                                target="_blank"
-                                href="https://www.youtube.com/watch?v=pDtUX3ZVHJ0"
-                            >
-                                <p>
-                                    <b>VIDEO</b> - Pixel Art Walk Cycle Time-lapse
-                                    and Tutorial
-                                </p>
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                rel="noreferrer"
-                                target="_blank"
-                                href="https://www.youtube.com/watch?v=xXEDKQ3wSfM"
-                            >
-                                <p>
-                                    <b>VIDEO</b> - Pixel Art "Bag Up" Animation
-                                    Time-lapse
-                                </p>
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                rel="noreferrer"
-                                target="_blank"
-                                href="https://www.youtube.com/watch?v=leZzb-Y0SKQ"
-                            >
-                                <p>
-                                    <b>VIDEO</b> - Pixel Art Bipedal Creature Run
-                                    Cycle Time-lapse
-                                </p>
-                            </a>
-                        </li>
-                    </ul>
-                    <br />
-                    <div className="captioned-image">
-                        <img src={gsts} alt="" />
-                        <p>
-                            <sub>
-                                <b>Figure 2:</b> My Own Little Project Car: 2016 Series 2 VF Commodore
-                            </sub>
-                        </p>
-                    </div>
-                    <br />
-                    <p>
-                        This page is currently a work in progress. I've recently
-                        found myself wanting to make more modifications to my car,
-                        and I have some project plans and ideas in the works. Expect
-                        updates to this page as I dive back into the world of car mods!
-                    </p>
-                    {/* <h3> Screen record time-lapses and make gifs</h3> */}
                 </div>
+                <p>
+                    I really started enjoying the work I was doing, and felt I
+                    was improving a lot. Around 2024, I started to even work on
+                    making a realistic racing simulator for driver training.
+                    This was just running Asseto Corsa, however was able to be
+                    customised into realistic environments and charecteristics.
+                </p>
+                <br />
             </div>
-        </div>
-            );
-            };
+            <div className="text-block">
+                <h2>@Blueyvf on TikTok</h2>
+                <br />
+                <p>
+                    Towards the end of highschool I started a tiktok account for
+                    my first car, a 2016 Series 2 VF Commodore. I enjoyed making
+                    videos, and it gave me an excuse to work on an take care of
+                    my car. Also, it was a great chance for me to explore a
+                    creative side I hadn't really explored before, and being
+                    able to teach myself about filming, cameras, and editing was
+                    a very fun phase in my live. None of my videos ever went
+                    particularly viral, with the exception of one that did over
+                    20 million views. You can check out my page {' '}
+                    <a
+                        rel="noreferrer"
+                        target="_blank"
+                        href="https://www.tiktok.com/@blueyvf?is_from_webapp=1&sender_device=pc"
+                    >
+                        here.
+                    </a>
+                    <br />
+                    <blockquote className="tiktok-embed" cite="https://www.tiktok.com/@blueyvf" data-unique-id="blueyvf"
+                                data-embed-type="creator" style="max-width: 780px; min-width: 288px;">
+                        <section><a target="_blank"
+                                    href="https://www.tiktok.com/@blueyvf?refer=creator_embed">@blueyvf</a></section>
+                    </blockquote>
+                    <script async src="https://www.tiktok.com/embed.js"></script>
+                </p>
+                <br />
+            </div>
+            <br />
 
-            export default ArtProjects;
+            <h3>Links:</h3>
+            <br />
+            <ul>
+                <li>
+                    <a
+                        rel="noreferrer"
+                        target="_blank"
+                        href="https://www.utsmotorsports.com/"
+                    >
+                        <p>
+                            <b>WEBSITE</b> - UTS Motorsports Official Website
+                        </p>
+                    </a>
+                </li>
+                <li>
+                    <a
+                        rel="noreferrer"
+                        target="_blank"
+                        href="https://www.tiktok.com/@blueyvf?is_from_webapp=1&sender_device=pc"
+                    >
+                        <p>
+                            <b>TIKTOK</b> - My page @Blueyvf
+                        </p>
+                    </a>
+                </li>
+            </ul>
+            <br />
+            <div className="captioned-image">
+                <img src={gsts} alt="" />
+                <p>
+                    <sub>
+                        <b>Figure 2:</b> My Own Little Project Car: 2016 Series 2 VF Commodore
+                    </sub>
+                </p>
+            </div>
+            <br />
+            <p>
+                This page is currently a work in progress. I've recently
+                found myself wanting to make more modifications to my car,
+                and I have some project plans and ideas in the works. Expect
+                updates to this page as I dive back into the world of car mods!
+            </p>
+            {/* <h3> Screen record time-lapses and make gifs</h3> */}
+        </div>
+)
+    ;
+};
+
+export default ArtProjects;
